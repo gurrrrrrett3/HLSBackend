f041c05e4357babcd39684c055cdffa5701628ee
Garrett Keneau <gart@gart.sh>
2024-07-24 10:54:08 -0400

    extra logging

--- src/webserver/indexRouter.ts
+++ src/webserver/indexRouter.ts
@@ -2,15 +2,19 @@ import { Router } from 'express';
 import LeaderboardManager from '../leaderboard/leaderboardManager.js';
 import { createHash } from 'crypto';
 import CryptoUtils from '../utils/crypto.js';
+import { Logger } from '../utils/logger.js';
 
 const indexRouter = Router();
+const logger = new Logger('Webserver');
 
 indexRouter.get('/leaderboard', (req, res) => {
+    logger.info('GET /leaderboard');
     res.json(LeaderboardManager.getLeaderboard());
 });
 
 indexRouter.post('/leaderboard', async (req, res) => {
     if (!req.body.username || !req.body.score) {
+        logger.error('POST /leaderboard', 'Missing username or score');
         return res.status(400).json({ error: 'Missing username or score' });
     }
 
@@ -20,6 +24,7 @@ indexRouter.post('/leaderboard', async (req, res) => {
         hashedIp: CryptoUtils.hashString((req.headers['x-forwarded-for'] as string) || (req.headers['x-real-ip'] as string) || req.ip || 'unknown')
     });
 
+    logger.info('POST /leaderboard', `Added entry for ${req.body.username}`);
     res.json({ success: true });
 });
 
